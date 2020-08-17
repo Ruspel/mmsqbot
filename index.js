21683f23ef80b60868df28531cd3e7d3b556dfb0
Ruspel <69706349+Ruspel@users.noreply.github.com>
2020-08-17 19:58:10 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on("guildMemberAdd", (member) => {
 
   welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
 
-  member.addRole(guild.roles.find(role => role.name == "Member"));
+  member.addRole(guild.roles.find(role => role.name == "Maniac Member"));
 });
 
 client.on("guildMemberRemove", (member) => {
@@ -29,7 +29,40 @@ client.on('message', (message) => {
 
   if(message.content === 'ping') {
     message.reply('pong');
-  }
+  };
+  if(message.content === '!help') {
+    let embed = new Discord.RichEmbed()
+    let commandList = [
+      {name: '!채광가이드', desc: '채광 관련 가이드 주소 공유'},
+      {name: '!운송가이드', desc: '운송 관련 가이드 주소 공유'},
+      {name: '!생산가이드', desc: '생산 관련 가이드 주소 공유'},
+      {name: '!복지가이드', desc: '코퍼레이션의 복지 안내'},
+    ];
+    let commandStr = '';
+    message.channel.send(embed)
+    let embed = new Discord.RichEmbed()
+      .setAuthor('Help of MMSQANSWERBOT', helpImg)
+      .setColor('#FE2E2E')
+      .setFooter(`명령어를 입력하세요.`)
+      .setTimestamp()
+      
+   //}// else if(message.content == '!채광가이드') {
+    //let embed = new Discord.RichEmbed()
+    //  .setAuthor('Help of 콜라곰 BOT', helpImg)
+    //  .setColor('#186de6')
+    //  .setFooter(`콜라곰 BOT ❤️`)
+    // .setTimestamp()
+      
+    commandList.forEach(x => {
+      commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
+    });
+  
+    embed.addField('Commands: ', commandStr);
+  
+    message.channel.send(embed)
+  };
 });
+
+
 
 client.login(token);
